abd859370ebd762974231fc3c96419c2e9c3f678
riffraffbacalso <rbacalso1024@gmail.com>
2023-03-16 17:55:06 +0000

    Set up video element

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script>
+  import Video from "./components/Video.vue";
+  export default {
+    components: { Video },
+  };
+</script>
+
+<template>
+  <Video />
+</template>
---
+++ src/components/Video.vue
@@ -0,0 +1,48 @@
+<script>
+  export default {
+    data() {
+      return {
+        paused: { type: Boolean },
+      };
+    },
+    methods: {
+      playPause() {
+        this.paused != this.paused;
+      },
+    },
+    mounted() {
+      let vid = this.$refs.vid;
+      console.log("video.paused:", vid.paused);
+      console.log(vid);
+    },
+  };
+</script>
+
+<template>
+  <div class="container-fluid position-relative">
+    <video
+      ref="vid"
+      class="object-fit-md-contain border w-100 h-100"
+      width="1280"
+      height="720"
+      autoplay
+      loop
+      muted
+    >
+      <source src="../assets/rocks.mp4" type="video/mp4" />
+    </video>
+    <p
+      :class="[
+        'position-absolute',
+        'bottom-0',
+        'start-50',
+        'translate-middle-x',
+        'text-center',
+        'fs-4',
+        'text-light',
+      ]"
+    >
+      It's interesting, the ghosts
+    </p>
+  </div>
+</template>
