73a052da74f4193efe79b0a5cf091c33d03c688a
riffraffbacalso <rbacalso1024@gmail.com>
2023-03-21 20:30:48 +0000

    Show video current time in progress bar

---
+++ src/components/Progress.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+    props: {
+      value: { type: Number },
+      min: { type: Number },
+      max: { type: Number },
+    },
+    computed: {
+      widthStyle() {
+        return `width: ${((this.value * 100) / this.max).toFixed(1)}%`;
+      },
+    },
+    mounted() {
+      // console.log("p", this.max);
+    },
+  };
+</script>
+
+<template>
+  <div class="progress bg-secondary" role="progressbar" style="height: 2px">
+    <div class="progress-bar bg-secondary-subtle" :style="widthStyle"></div>
+  </div>
+</template>
--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,17 +1,32 @@
 <script>
   import PlayPauseButton from "./PlayPauseButton.vue";
+  import Progress from "./Progress.vue";
 
   export default {
     components: {
       PlayPauseButton,
+      Progress,
     },
     data() {
       return {
         vid: null,
         paused: null,
+        currentTime: null,
+        duration: null,
       };
     },
     methods: {
+      onLoad() {
+        this.duration = +this.vid.duration.toFixed(1);
+      },
+      onUpdate() {
+        requestAnimationFrame(
+          () => (this.currentTime = +this.vid.currentTime.toFixed(1))
+        );
+      },
+      onEnd() {
+        this.paused = true;
+      },
       playPause() {
         if (this.vid.paused) this.vid.play();
         else this.vid.pause();
@@ -21,6 +36,7 @@
     mounted() {
       this.vid = this.$refs.vid;
       this.paused = this.vid.paused;
+      this.currentTime = this.vid.currentTime;
     },
   };
 </script>
@@ -28,7 +44,13 @@
 <template>
   <div class="container-fluid w-50 h-50">
     <div class="container-fluid d-flex position-relative w-100 h-100 pb-2 px-0">
-      <video ref="vid" class="object-fit-md-contain w-100 h-100" loop muted>
+      <video
+        ref="vid"
+        class="object-fit-md-contain w-100 h-100"
+        @loadedmetadata="onLoad"
+        @timeupdate="onUpdate"
+        @ended="onEnd"
+      >
         <source src="../assets/rocks.mp4" type="video/mp4" />
       </video>
       <p
@@ -45,6 +67,13 @@
         It's interesting, the ghosts
       </p>
     </div>
-    <PlayPauseButton :paused="paused" :playPause="playPause" />
+    <div class="row mx-0">
+      <div class="col-sm-auto px-0">
+        <PlayPauseButton :paused="paused" :playPause="playPause" />
+      </div>
+      <div class="col align-self-center px-0 mx-1">
+        <Progress :value="currentTime" :min="0" :max="duration" label="" />
+      </div>
+    </div>
   </div>
 </template>
