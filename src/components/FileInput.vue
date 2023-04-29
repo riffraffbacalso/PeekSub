2371d896f4b9882e6623ad34fffbdfd8fe253650
riffraffbacalso <rbacalso1024@gmail.com>
2023-04-29 19:24:30 +0100

    Prevent word wrap on file inputs

--- src/components/FileInput.vue
+++ src/components/FileInput.vue
@@ -39,17 +39,16 @@
 
   label {
     display: inline-block;
+    overflow: hidden;
     width: calc(50% - 45px);
     padding: 5px 15px;
     border-radius: 2px;
-    margin: 10px;
     background-color: #3f4e4f;
     color: lightgray;
     cursor: pointer;
     font-size: 0.75rem;
     text-align: center;
-  }
-
-
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 </style>
